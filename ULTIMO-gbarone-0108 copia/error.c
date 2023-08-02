50e0c0c8582db4a23b66247d2c476e872041359e
gbarone42 <badph3@gmail.com>
2023-08-02 04:09:22 +0200

    j

--- ULTIMO-gbarone-0108 copia/error.c
+++ ULTIMO-gbarone-0108 copia/error.c
@@ -1,21 +1,21 @@
 void ft_error(void)
 {
-    printf("\n");
-    printf("     /\\_/\\     \n");
-    printf("    ( o.o )    \n");
-    printf("    > ^ <      \n");
-    printf("\n");
-    printf("\n\033[1;31mError\033[0m\n\n");
-    exit(1);
+    ft_printf("\n");
+    ft_printf("     /\\_/\\     \n");
+    ft_printf("    ( o.o )    \n");
+    ft_printf("    > ^ <      \n");
+    ft_printf("\n");
+    ft_printf("\n\033[1;31mError\033[0m\n\n");
+    ft_exit(1);
 }
 
 void ft_error_few_arg(void)
 {
-    printf("\n");
-    printf("     /\\_/\\     \n");
-    printf("    ( o.o )    \n");
-    printf("    > ^ <      \n");
-    printf("\n");
-    printf("\n\033[1;31mError: TOO-FEW-ARGS\033[0m\n\n");
+    ft_printf("\n");
+    ft_printf("     /\\_/\\     \n");
+    ft_printf("    ( o.o )    \n");
+    ft_printf("    > ^ <      \n");
+    ft_printf("\n");
+    ft_printf("\n\033[1;31mError: TOO-FEW-ARGS\033[0m\n\n");
     exit(1);
 }
