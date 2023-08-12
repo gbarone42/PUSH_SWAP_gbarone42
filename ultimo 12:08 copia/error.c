2fe2219e861a95d0e9aaa93c6265e32df2cc2d9c
gbarone42 <badph3@gmail.com>
2023-08-12 10:23:33 +0200

    j

---
+++ ultimo 12:08 copia/error.c
@@ -0,0 +1,119 @@
+#include "push_swap.h"
+
+
+void ft_error_few_arg(void)
+{
+    write(1, "\n\033[1;31mError\033[0m\n\n", 20);
+        write(1, "\n\033[1;31mError\033[0m\n\n", 20);
+    write(1, "        4444444       \n", 24);
+    write(1, "       4       2      \n", 24);
+    write(1, "      4               \n", 24);
+    write(1, "     4444444          \n", 24);
+    write(1, "            22        \n", 24);
+    write(1, "            2 2       \n", 24);
+    write(1, "      4444444         \n", 24);
+    write(1, "          2           \n", 24);
+    write(1, "         2            \n", 24);
+    write(1, "        22222         \n", 24);
+    write(1, "\n\033[1;31mError00\033[0m\n\n", 22);
+    write(1, "\n", 2);
+    exit(1);
+}
+
+void ft_error(void)
+{
+    write(1, "\n", 1);
+    write(1, "\n\033[1;31mError\033[0m\n\n", 19);
+    write(1, "     /\\_/\\     \n", 16);
+    write(1, "    ( o.o )    \n", 16);
+    write(1, "    > ^ <    42\n", 16);
+    write(1, "\n", 1);
+    write(1, "\n\033[1;31mError01\033[0m\n\n", 21);
+    write(1, "\n", 1);
+    exit(1);
+}
+
+
+void ft_errorr(void)
+{
+    write(1, "\n", 1);
+    write(1, "\n\033[1;31mError\033[0m\n\n", 19);
+    write(1, "      ><(((°>      \n", 22);
+    write(1, "    <°)))><       \n", 21);
+    write(1, "   >°)))><        \n", 20);
+    write(1, "  <°)))><         \n", 19);
+    write(1, " ><°)))><          \n", 18);
+    write(1, "       ><(((°>    \n", 22);
+    write(1, " 42      <°)))><  \n", 21);
+    write(1, "\n\033[1;31mError2\033[0m\n\n", 21);
+    write(1, "\n", 1);
+    exit(1);
+}
+
+void ft_errorrr(void)
+{
+    write(1, "\n", 1);
+    write(1, "\n\033[1;31mError\033[0m\n\n", 19);
+     write(1, " 4       <°)))><  \n", 21);
+    write(1, "    /\\_/\\     \n", 17);
+    write(1, "   ( o.o )    \n", 16);
+    write(1, "   (> ^ <)      <°)))><  \n", 28);
+    write(1, "  /      \\   2  \n", 18);
+    write(1, " / /~~~~\\ \\  \n", 18);
+    write(1, "/_/      \\_\\   \n", 20);
+    write(1, "\n", 1);
+    write(1, "\n\033[1;31mError03\033[0m\n\n", 21);
+    write(1, "\n", 1);
+    exit(1);
+}
+
+void ft_errorrrr(void)
+{
+  write(1, "\n", 2);
+    write(1, "   <°)))><       \n", 20);
+    write(1, "\n\033[1;31mError\033[0m\n\n", 20);
+    write(1, "     /\\_/\\     \n", 17);
+    write(1, "    ( o.o )    \n", 17);
+    write(1, "    > ^ <    42\n", 17);
+    write(1, "\n", 2);
+    write(1, "\n\033[1;31mError04\033[0m\n\n", 22);
+    write(1, "\n", 1);
+    exit(1);
+}
+
+void ft_errorrrrr(void)
+{
+    write(1, "\n", 1);
+    write(1, "\n\033[1;31mError\033[0m\n\n", 19);
+    write(1, "     ><(((°>    \n", 19);
+    write(1, "       <°)))><  \n", 18);
+    write(1, "     /\\___/\\      \n", 21);
+    write(1, " /  O O  \\       \n", 19);
+    write(1, "( == ^ == )    2 \n", 19);
+    write(1, "   )      (     \n", 17);
+    write(1, "  /        \\   \n", 18);
+    write(1, " /          \\  \n", 19);
+    write(1, "/_ _ _ _ _ _\\ \n", 18);
+    write(1, "     ><(((°>    \n", 19);
+    write(1, " 4     <°)))><  \n", 18);
+    write(1, "\n\033[1;31mError05\033[0m\n\n", 21);
+    write(1, "\n", 1);
+    exit(1);
+}
+
+void print_stack(t_stack *stack)
+{
+    printf("Stack: ");
+    t_stack *current = stack;
+    while (current != NULL)
+    {
+        printf("%d ", current->content);
+        current = current->next;
+    }
+    printf("\n");
+}
+
+void print_max_digit(int max_digit) 
+{
+    printf("Max Digit: %d\n", max_digit);
+}
---
+++ ultimo 12:08 copia/sort.c
@@ -0,0 +1,223 @@
+#include "push_swap.h"
+/*
+void	ft_sa(t_stack **stack_a)
+{
+	t_stack	*tmp;
+	t_stack	*tmp2;
+
+	tmp = *stack_a;
+	tmp2 = tmp->next;
+	*stack_a = tmp2;
+	tmp->next = tmp2->next;
+	tmp2->next = tmp;
+	write(1, "sa\n", 3);
+	return ;
+}
+
+void	ft_pa(t_stack **stack_b, t_stack **stack_a)
+{
+	t_stack	*tmp;
+
+	if (*stack_b == NULL)
+		return ;
+	tmp = *stack_b;
+	*stack_b = (*stack_b)->next;
+	tmp->next = *stack_a;
+	*stack_a = tmp;
+	write(1, "pa\n", 3);
+	return ;
+}
+
+void	ft_pb(t_stack **stack_a, t_stack **stack_b)
+{
+	t_stack	*tmp;
+
+	if (*stack_a == NULL)
+		return ;
+	tmp = *stack_a;
+	*stack_a = (*stack_a)->next;
+	tmp->next = *stack_b;
+	*stack_b = tmp;
+	write(1, "pb\n", 3);
+	return ;
+}
+
+void	ft_ra(t_stack **stack_a)
+{
+	t_stack	*last;
+	t_stack	*first;
+
+	if (*stack_a == NULL || (*stack_a)->next == NULL)
+		return ;
+	last = *stack_a;
+	first = *stack_a;
+	while (last->next != NULL)
+		last = last->next;
+	*stack_a = first->next;
+	first->next = NULL;
+	last->next = first;
+	write(1, "ra\n", 3);
+	return ;
+}
+
+*/
+void	ft_rra(t_stack **stack_a)
+{
+	t_stack	*last;
+	t_stack	*tmp;
+
+	last = *stack_a;
+	tmp = *stack_a;
+	while (last->next != NULL)
+	{
+		tmp = last;
+		last = last->next;
+	}
+	last->next = *stack_a;
+	*stack_a = last;
+	tmp->next = NULL;
+	write(1, "rra\n", 4);
+}
+
+
+
+void	ft_sb(t_stack **stack_b)
+{
+	t_stack	*tmp;
+	t_stack	*tmp2;
+
+	tmp = *stack_b;
+	tmp2 = tmp->next;
+	*stack_b = tmp2;
+	tmp->next = tmp2->next;
+	tmp2->next = tmp;
+	write(1, "sb\n", 3);
+	return ;
+}
+
+void	ft_ss(t_stack **stack_a, t_stack **stack_b)
+{
+	t_stack	*tmp;
+	t_stack	*tmp2;
+
+	tmp = *stack_a;
+	tmp2 = tmp->next;
+	*stack_a = tmp2;
+	tmp->next = tmp2->next;
+	tmp2->next = tmp;
+	tmp = NULL;
+	tmp2 = NULL;
+	tmp = *stack_b;
+	tmp2 = tmp->next;
+	*stack_b = tmp2;
+	tmp->next = tmp2->next;
+	tmp2->next = tmp;
+	write(1, "ss\n", 3);
+	return ;
+}
+
+void	ft_sa(t_stack **stack_a)
+{
+	t_stack	*tmp;
+	t_stack	*tmp2;
+
+	tmp = *stack_a;
+	tmp2 = tmp->next;
+	*stack_a = tmp2;
+	tmp->next = tmp2->next;
+	tmp2->next = tmp;
+	write(1, "sa\n", 3);
+	return ;
+}
+
+void	ft_rb(t_stack **stack_b)
+{
+	t_stack	*last;
+	t_stack	*first;
+
+	if (*stack_b == NULL || (*stack_b)->next == NULL)
+		return ;
+	last = *stack_b;
+	first = *stack_b;
+	while (last->next != NULL)
+		last = last->next;
+	*stack_b = first->next;
+	first->next = NULL;
+	last->next = first;
+	write(1, "rb\n", 3);
+	return ;
+}
+
+void	ft_rr(t_stack **stack_a, t_stack **stack_b)
+{
+	t_stack	*last;
+	t_stack	*first;
+
+	if (*stack_a == NULL || (*stack_a)->next == NULL)
+		return ;
+	last = *stack_a;
+	first = *stack_a;
+	while (last->next != NULL)
+		last = last->next;
+	*stack_a = first->next;
+	first->next = NULL;
+	last->next = first;
+	if (*stack_b == NULL || (*stack_b)->next == NULL)
+		return ;
+	last = *stack_b;
+	first = *stack_b;
+	while (last->next != NULL)
+		last = last->next;
+	*stack_b = first->next;
+	first->next = NULL;
+	last->next = first;
+	write(1, "rr\n", 3);
+	return ;
+}
+
+void	ft_rrb(t_stack **stack_b)
+{
+	t_stack	*last;
+	t_stack	*tmp;
+
+	last = *stack_b;
+	tmp = *stack_b;
+	while (last->next != NULL)
+	{
+		tmp = last;
+		last = last->next;
+	}
+	last->next = *stack_b;
+	*stack_b = last;
+	tmp->next = NULL;
+	write(1, "rrb\n", 4);
+}
+
+void	ft_rrr(t_stack **stack_a, t_stack **stack_b)
+{
+	t_stack	*last;
+	t_stack	*tmp;
+
+	last = *stack_a;
+	tmp = *stack_a;
+	while (last->next != NULL)
+	{
+		tmp = last;
+		last = last->next;
+	}
+	last->next = *stack_a;
+	*stack_a = last;
+	tmp->next = NULL;
+	last = *stack_b;
+	tmp = *stack_b;
+	while (last->next != NULL)
+	{
+		tmp = last;
+		last = last->next;
+	}
+	last->next = *stack_b;
+	*stack_b = last;
+	tmp->next = NULL;
+	write(1, "rrr\n", 4);
+	return ;
+}
